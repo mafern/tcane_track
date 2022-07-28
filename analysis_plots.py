af106908b164202dd00f7f72d50768220de9690a
Elizabeth Barnes <eabarnes@colostate.edu>
2022-07-28 13:10:07 -0600

    copied intensity code over

---
+++ analysis_plots.py
@@ -0,0 +1,236 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import model_diagnostics
+
+# import imp
+# imp.reload(model_diagnostics)
+
+colors = ('#284E60','#E1A730','#D95980','#C3B1E1','#351F27','#A9C961')
+clr_shash = colors[0]
+clr_bnn   = colors[3]
+clr_truth = 'dimgray'
+
+
+
+FS = 16
+### for white background...
+plt.rc('text',usetex=True)
+plt.rc('font',**{'family':'sans-serif','sans-serif':['Avant Garde']}) 
+plt.rc('savefig',facecolor='white')
+plt.rc('axes',facecolor='white')
+plt.rc('axes',labelcolor='dimgrey')
+plt.rc('axes',labelcolor='dimgrey')
+plt.rc('xtick',color='dimgrey')
+plt.rc('ytick',color='dimgrey')
+
+def adjust_spines(ax, spines):
+    for loc, spine in ax.spines.items():
+        if loc in spines:
+            spine.set_position(('outward', 5))
+        else:
+            spine.set_color('none')  
+    if 'left' in spines:
+        ax.yaxis.set_ticks_position('left')
+    else:
+        ax.yaxis.set_ticks([])
+    if 'bottom' in spines:
+        ax.xaxis.set_ticks_position('bottom')
+    else:
+            ax.xaxis.set_ticks([])  
+
+def plot_sample(ax, onehot_val, shash_incs, shash_cpd, bnn_cpd, sample=130):
+    plt.sca(ax)  
+
+    if(shash_cpd.shape[0]<sample):
+        sample = shash_cpd.shape[0]-1
+    
+    bins = np.arange(np.min(shash_incs),np.max(shash_incs)+2,2)
+
+    # results for SHASH
+    plt.plot(shash_incs,
+             shash_cpd[sample,:],
+             color=clr_shash,
+             linewidth=4,
+             label='SHASH',
+            )
+
+    # results for BNN
+    if bnn_cpd is not None:
+        plt.hist(bnn_cpd[sample,:],
+                 bins=bins,
+                 histtype=u'step',
+                 density=True, 
+                 color=clr_bnn,
+                 linewidth=2,
+                 label='BNN',
+                )
+
+    # truth
+    plt.axvline(x=onehot_val[sample,0],color=clr_truth,linestyle='--')
+    plt.text(x=onehot_val[sample,0]+1, 
+             y = .09, 
+             s = 'truth', 
+             color=clr_truth,
+             fontsize=12,
+             horizontalalignment='left',
+            )
+    plt.legend()
+    
+    plt.ylim(0,0.1)
+    plt.xlim(-45,45)
+
+    ax = plt.gca()
+    xticks = ax.get_xticks()
+    yticks = np.around(ax.get_yticks(),2)
+    ax.set_xticks(xticks.astype(int),xticks.astype(int))
+    ax.set_yticks(yticks,yticks)
+
+    plt.title('validation sample ' + str(sample))
+    plt.xlabel('predicted deviation from consensus (knots)')
+    plt.ylabel('probability density function')    
+
+    
+    
+    
+def plot_pits(ax, x_val, onehot_val, model_shash, shash_cpd, bnn_cpd):
+    plt.sca(ax)      
+    
+    # shash pit
+    bins, hist_shash, D_shash, EDp_shash = model_diagnostics.compute_pit('shash',onehot_val, x_data=x_val,model_shash=model_shash)
+    bins_inc = bins[1]-bins[0]
+
+    if bnn_cpd is not None:
+        bin_add = bins_inc/6+bins_inc/6 
+        bin_width = bins_inc/3
+    else:
+        bin_add = bins_inc/2
+        bin_width = bins_inc*.98
+    plt.bar(hist_shash[1][:-1] + bin_add,
+             hist_shash[0],
+             width=bin_width,
+             color=clr_shash,
+             label='SHASH',
+            )
+    
+    # bnn pit    
+    if bnn_cpd is not None:
+        bins, hist_bnn, D_bnn, EDp_bnn = model_diagnostics.compute_pit('bnn',onehot_val, bnn_cpd)
+        plt.bar(hist_bnn[1][:-1]+3*bins_inc/6+bins_inc/6,
+                 hist_bnn[0],
+                 width=bins_inc/3,
+                 color=clr_bnn,
+                 label='BNN',
+                )
+        plt.text(0.,np.max(yticks)*.94,
+                 'BNN ~~~~D: ' + str(np.round(D_bnn,4)) + ' (' + str(np.round(EDp_bnn,3)) +  ')', 
+                 color=clr_bnn,
+                 verticalalignment='top',             
+                 fontsize=12)
+        
+
+    # make the figure pretty
+    plt.axhline(y=.1, 
+                linestyle='--',
+                color='dimgray', 
+                linewidth=2.,
+               )
+    plt.ylim(0,.2)
+    plt.xticks(bins,np.around(bins,1))
+    ax = plt.gca()
+    yticks = np.around(np.arange(0,.25,.05),2)
+    plt.yticks(yticks,yticks)
+    
+    plt.text(0.,np.max(yticks)*.99,
+             'SHASH D: ' + str(np.round(D_shash,4)) + ' (' + str(np.round(EDp_shash,3)) +  ')', 
+             color=clr_shash,     
+             verticalalignment='top',
+             fontsize=12)
+
+
+    plt.xlabel('probability integral transform')
+    plt.ylabel('probability')
+    plt.legend(loc=1)
+    plt.title('PIT histogram comparison', fontsize=FS, color='k')
+    
+def plot_medians(ax, onehot_val, shash_cpd, bnn_cpd, shash_med):
+    plt.sca(ax)
+    
+    bnn_med = np.median(bnn_cpd,axis=1)
+    shash_error = np.mean(np.abs(shash_med - onehot_val[:,0]))
+    bnn_error = np.mean(np.abs(bnn_med - onehot_val[:,0]))
+
+    plt.plot(shash_med,
+             bnn_med,
+             linestyle='None',
+             marker='.',
+             color='dimgray',
+             markerfacecolor='violet',
+             markersize=7,
+             markeredgewidth=.5,
+            )
+
+    plt.xlabel('SHASH median')
+    plt.ylabel('BNN median')
+
+    plt.plot((-100,100),(-100,100),'--',color='dimgray', linewidth=1.)
+    plt.axis('scaled')
+    plt.xlim(-25,25)
+    plt.ylim(-25,25)
+
+    ax = plt.gca()
+    xticks = ax.get_xticks()
+    yticks = ax.get_yticks()
+    plt.xticks(xticks.astype(int),xticks.astype(int))
+    plt.yticks(yticks.astype(int),yticks.astype(int))
+
+    plt.text(-29,27,
+             'SHASH mean $|$error$|$: ' + str(np.round(shash_error,2)), 
+             color='dimgray',         
+             fontsize=12)
+    plt.text(-29,24,
+             'BNN ~~~~mean $|$error$|$: ' + str(np.round(bnn_error,2)), 
+             color='dimgray',
+             fontsize=12)
+
+    plt.title('Median vs Median')    
+    
+def plot_nlls(ax, x_val, onehot_val, model_shash, shash_cpd, bnn_cpd):
+    plt.sca(ax)
+    
+    shash_nloglike = model_diagnostics.compute_nll('shash', onehot_val, model_shash=model_shash, x_val=x_val)
+    bnn_nloglike   = model_diagnostics.compute_nll('bnn', onehot_val, bnn_cpd=bnn_cpd)
+    plt.plot(shash_nloglike.numpy(),
+             bnn_nloglike,
+             linestyle='None',
+             marker='.',
+             color='dimgray',
+             markerfacecolor='violet',
+             markersize=7,
+             markeredgewidth=.5,
+            )
+
+    plt.xlabel('SHASH negative log-likelihood')
+    plt.ylabel('BNN negative log-likelihood')
+
+    plt.plot((-100,100),(-100,100),'--',color='dimgray', linewidth=1.)
+    plt.axis('scaled')
+    plt.xlim(2,11)
+    plt.ylim(2,11)
+
+    ax = plt.gca()
+    xticks = ax.get_xticks()
+    yticks = ax.get_yticks()
+    plt.xticks(xticks.astype(int),xticks.astype(int))
+    plt.yticks(yticks.astype(int),yticks.astype(int))
+
+    plt.text(2.1,10.6,
+             'SHASH mean NLL: ' + str(np.round(np.nanmean(shash_nloglike.numpy()),3)), 
+             color='dimgray',         
+             fontsize=12)
+    plt.text(2.1,10.2,
+             'BNN ~~~~mean NLL: ' + str(np.round(np.nanmean(bnn_nloglike),3)),
+             color='dimgray',
+             fontsize=12)
+
+    plt.title('NLL Comparison')    
+        
---
+++ build_data.py
@@ -0,0 +1,317 @@
+"""Build the split and scaled training and validation hurricane data arrays.
+
+Functions
+---------
+build_hurricane_data(data_path, settings, verbose=0)
+
+"""
+import pprint
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import copy
+
+import toolbox
+
+
+__author__ = "Elizabeth A. Barnes and Randal J Barnes"
+__version__ = "18 March 2022"
+
+
+def build_hurricane_data(data_path, settings, verbose=0):
+    """Build the training and validation tensors.
+
+    The settings['target'] specifies which data set to build.There are five
+    different possible targets: intensity, logitude, latitude, radial, and
+    angle.
+
+    Arguments
+    ---------
+    data_path : str
+        The input filepath, not including the file name.
+
+    settings : dict
+        The parameters defining the current experiment.
+
+    verbose : int
+        0 -> silent
+        1 -> description only
+        2 -> description and y statistics
+
+    Returns
+    -------
+    x_train : numpy.ndarray
+        The training split of the x data.
+        shape = [n_train, n_features].
+
+    onehot_train : numpy.ndarray
+        The training split of the scaled y data is in the first column.
+        The remaining columns are filled with zeros. The number of columns
+        equal the number of distribution parameters.
+        shape = [n_train, n_parameters].
+
+    x_val : numpy.ndarray
+        The validation split of the x data.
+        shape = [n_val, n_features].
+
+    onehot_val : numpy.ndarray
+        The validation split of the scaled y data is in the first column.
+        The remaining columns are filled with zeros. The number of columns
+        equal the number of distribution parameters.
+        shape = [n_val, n_parameters].
+
+    data_summary : dict
+        A descriptive dictionary of the data.
+
+    df_val : pandas dataframe
+        A pandas dataframe containing validation records.  The dataframe
+        contains all columns from the original file.
+        However, the dataframe contains only rows from the validation data
+        set that satisfy the specified basin and leadtime requirements, and
+        were not eliminated due to missing values.
+
+        The dataframe has the shuffled order of the rows.  In particular,
+        the rows of df_val align with the rows of x_val and onehot_val.
+
+    Notes
+    -----
+    * No scaling or normalization is applied during data preparation.
+
+    """
+    # Setup for the selected target.
+    if settings["target"] == "intensity":
+        x_names = [
+            "NCI","VMAX0",
+            "DSDV", "LGDV", "HWDV", "AVDV",
+            "VMXC", "DV12", "SLAT", "SSTN", "SHDC", "DTL",
+        ]
+        y_name = ["OBDV"]
+        missing = None
+
+    elif settings["target"] == "longitude":
+        # x_names = ["AVDX", "EMDX", "EGDX", "HWDX", "LONC"]
+        x_names = [
+            "NCT","VMAX0",
+            "AVDX", "EMDX", "EGDX", "HWDX",
+            "LONC", "LATC",
+            "VMXC", "DV12", "SLAT", "SHDC", "SSTN", "DTL",
+        ]
+        y_name = ["OBDX"]
+        missing = -9999
+
+    elif settings["target"] == "latitude":
+        # x_names = ["AVDY", "EMDY", "EGDY", "HWDY", "LATC"]
+        x_names = [
+            "NCT", "VMAX0",
+            "AVDY", "EMDY", "EGDY", "HWDY",
+            "LONC", "LATC",
+            "VMXC", "DV12", "SLAT", "SHDC", "SSTN", "DTL",
+        ]
+        y_name = ["OBDY"]
+        missing = -9999
+
+    elif settings["target"] == "radial":
+        x_names = [
+            "NCT",
+            "AVDX", "EMDX", "EGDX", "HWDX",
+            "AVDY", "EMDY", "EGDY", "HWDY",
+            "LONC", "LATC",
+            "VMXC", "DV12", "SHDC", "SSTN", "DTL",
+            "DSDV", "LGDV", "HWDV", "AVDV",
+        ]
+        y_name = ["OBDR"]
+        missing = -9999
+
+    else:
+        raise NotImplementedError
+
+    # Get the data from the specified file and filter out the unwanted rows.
+    datafile_path = data_path + settings["filename"]
+    df_raw = pd.read_table(datafile_path, sep="\s+")
+    df_raw = df_raw.rename(columns={'Date': 'year'})    
+
+    df = df_raw[
+        (df_raw["ATCF"].str.contains(settings["basin"])) &
+        (df_raw["ftime(hr)"] == settings["leadtime"])
+    ]
+
+    if missing is not None:
+        df = df.drop(df.index[df[y_name[0]] == missing])
+
+    # Shuffle the rows in the df Dataframe, using the numpy rng.
+    # rng = np.random.default_rng(settings['rng_seed'])
+    df = df.sample(frac=1,random_state=settings['rng_seed'])
+    df = df.reset_index(drop=True)
+
+    #======================================================================
+    # Train/Validation/Test Split
+    
+    # Get the testing data
+    if settings["test_condition"] is None:
+        pass
+    elif settings["test_condition"] == "cluster":
+        
+        from scipy.cluster.vq import kmeans,vq
+        numclust = 6
+        
+        data = np.copy(df[x_names].to_numpy())
+        data_mean = np.mean(data,axis=0)
+        data_std  = np.std(data,axis=0)
+        data = (data - data_mean)/data_std
+
+        clusters, dist = kmeans(data, numclust, iter=500, seed=settings["rng_seed"])
+        cluster_label, _ = vq(data,clusters)
+        class_freq = np.bincount(cluster_label)
+        cluster_out = np.argmin(class_freq)
+
+        index = np.where(cluster_label == cluster_out)[0]
+        df_test = df.iloc[index]
+        x_test = df_test[x_names].to_numpy()
+        y_test = np.squeeze(df_test[y_name].to_numpy())
+        df_test = df_test.reset_index(drop=True)
+        
+        df = df.drop(index)
+        df = df.reset_index(drop=True)
+        
+        if verbose != 0:
+            fig, axs = plt.subplots(1,2, figsize=(15,5))
+            plt.sca(axs[0])
+            plt.hist(cluster_label,np.arange(-.5,numclust+.5,1.), width=.98)
+            plt.title('Sample Count by Cluster')
+            plt.ylabel('number of samples')
+            plt.xlabel('cluster')
+            plt.xticks((0,1,2,3))
+            plt.sca(axs[1])
+            for ic in np.arange(0,numclust):
+                plt.plot(x_names,clusters[ic,:], label='cluster ' + str(ic),linewidth=2)
+            plt.legend()
+            plt.title('Cluster Centroid')
+            plt.ylabel('standardized units')
+            plt.xlabel('predictor')
+            plt.show() 
+    else:
+        years = settings["years_test"]
+        if verbose != 0:
+            print('years' + str(years) + ' withheld for testing')
+        index = df.index[df['year'].isin(years)]   
+        df_test = df.iloc[index]
+        x_test = df_test[x_names].to_numpy()
+        y_test = np.squeeze(df_test[y_name].to_numpy())
+        df_test = df_test.reset_index(drop=True)
+        
+        df = df.drop(index)
+        df = df.reset_index(drop=True)
+        
+    # get the validation data
+    if settings["val_condition"] == "random":
+        index = np.arange(0,settings["n_val"])
+        if(len(index)<100):
+            raise Warning("Are you sure you want n_val < 100?")
+            
+    elif settings["val_condition"] == "years":
+        if verbose != 0:
+            print('years' + str(settings["n_val"]) + ' withheld for testing')
+        index = df.index[df['year'].isin(settings["n_val"])]   
+        
+        # unique_years = df['year'].unique()
+        # years = unique_years[:settings["n_val"]]
+        # index = df.index[df['year'].isin(years)] 
+        
+    df_val = df.iloc[index]
+    x_val = df_val[x_names].to_numpy()
+    y_val = np.squeeze(df_val[y_name].to_numpy())
+    df_val = df_val.reset_index(drop=True)
+    
+    df = df.drop(index)
+    df = df.reset_index(drop=True)
+    
+    if settings["test_condition"] is None:
+        df_test = df_val.copy()
+        x_test  = copy.deepcopy(x_val)
+        y_test  = copy.deepcopy(y_val)
+    
+    # Subsample training if desired
+    if settings["n_train"] == "max":
+        df_train = df.copy()
+    else:
+        df_train = df.iloc[:settings["n_train"]]
+    x_train = df_train[x_names].to_numpy()
+    y_train = np.squeeze(df_train[y_name].to_numpy())
+    df_train = df_train.reset_index(drop=True)
+    
+    #======================================================================    
+    # Create 'onehot' y arrays. The y values go in the first column, and the
+    # remaining columns are zero -- i.e. dummy columns.  These dummy columns
+    # are required by tensorflow; the number of columns must equal the number
+    # of distribution parameters.
+    if settings["uncertainty_type"] in ("bnn","mcdrop","reg"):
+        n_parameters = 1
+    elif "bnnshash" in settings["uncertainty_type"]:
+        n_parameters = 1    
+    elif "shash2" in settings["uncertainty_type"]:
+        n_parameters = 2
+    elif "shash3" in settings["uncertainty_type"]:
+        n_parameters = 3
+    elif "shash4" in settings["uncertainty_type"]:
+        n_parameters = 4
+    else:
+        raise NotImplementedError
+    
+           
+    onehot_train = np.zeros((len(y_train), n_parameters))
+    onehot_val = np.zeros((len(y_val), n_parameters))
+    onehot_test = np.zeros((len(y_test), n_parameters))    
+
+    onehot_train[:, 0] = y_train
+    onehot_val[:, 0] = y_val
+    onehot_test[:, 0] = y_test
+
+    # Make a descriptive dictionary.
+    data_summary = {
+        "datafile_path": datafile_path,
+        "x_train_shape": tuple(x_train.shape),
+        "x_val_shape": tuple(x_val.shape),
+        "x_test_shape": tuple(x_test.shape),        
+        "onehot_train_shape": tuple(onehot_train.shape),
+        "onehot_val_shape": tuple(onehot_val.shape),
+        "onehot_test_shape": tuple(onehot_test.shape),        
+        "x_names": x_names,
+        "y_name": y_name,
+    }
+
+    # Report the results.
+    if verbose >= 1:
+        pprint.pprint(data_summary, width=80)
+
+    if verbose >= 2:
+        toolbox.print_summary_statistics({"y_train" : onehot_train[:,0], 
+                                          "y_val"   : onehot_val[:,0], 
+                                          "y_test"  : onehot_test[:,0]}, 
+                                         sigfigs=1)
+        
+    # change dtype of onehot
+    onehot_train = onehot_train.astype('float32')
+    onehot_val = onehot_val.astype('float32')    
+    onehot_test = onehot_test.astype('float32')
+    
+    # create valtest set
+    x_valtest = np.concatenate((x_val, x_test), axis=0)
+    onehot_valtest = np.concatenate((onehot_val, onehot_test), axis=0)
+    df_valtest = df_val.append(df_test)
+
+    return (
+        data_summary,        
+        x_train,
+        onehot_train,
+        x_val,
+        onehot_val,
+        x_test,
+        onehot_test,        
+        x_valtest,
+        onehot_valtest,
+        df_train,
+        df_val,
+        df_test,
+        df_valtest,
+    )
