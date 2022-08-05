3a8e6a5c00a7ab1ce55f40f41ab15fff3009372a
Randal Barnes <barne003@umn.edu>
2022-08-05 15:27:52 -0500

    added Mahalanobis cdf methods

---
+++ bivariate_normal/mahalanobis.py
@@ -0,0 +1,117 @@
+"""Tools for the Mahalanobis cdf using a bivariate normal.
+
+Functions
+---------
+plot_cdf(mu_u, mu_v, sigma_u, sigma_v, rho)
+compute_cdf(mu_u, mu_v, sigma_u, sigma_v, rho, y)
+
+Notes
+-----
+* The Mahalanobis cdf is defined in the "Cumulative distribution
+    function" section of [1].
+    
+* The Mahalanobis cdf is the probability that a sample lies inside
+    the ellipse determined by its Mahalanobis distance.
+
+References
+----------
+[1] Wikipedia contributors. (2022, July 20). Multivariate normal 
+    distribution. In Wikipedia, The Free Encyclopedia. Retrieved 
+    19:53, August 5, 2022.
+
+[2] Wikipedia contributors. (2022, June 21). Mahalanobis distance.
+    In Wikipedia, The Free Encyclopedia. Retrieved 19:55, August 5, 2022.
+
+[3] Michael Bensimhoun. (2009, June). N-dimension Cumulative Function
+    and Other Useful Facts About Gaussians and Normal Densities.
+    https://upload.wikimedia.org/wikipedia/commons/a/a2/Cumulative_function_n_dimensional_Gaussians_12.2013.pdf
+
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+__author__ = "Randal J Barnes and Elizabeth A. Barnes"
+__version__ = "05 August 2022"
+
+
+COLOR = ('#8dd3c7', '#ffffb3', '#bebada', '#fb8072', '#80b1d3', '#fdb462', '#b3de69', '#fccde5', '#d9d9d9')
+THETA = np.linspace(0, 2*np.pi, 1000);
+
+
+def plot_cdf(mu_u, mu_v, sigma_u, sigma_v, rho):
+    """Plot the eliptical contours of the Mahalanobis cdf.
+    
+    Arguments
+    ---------
+    mu_u : float
+        mean of u.
+    
+    mu_v : float
+        mean of v.
+    
+    sigma_u : float, u > 0.
+        standard deviation of u.
+    
+    sigma_v : float, v > 0.
+        standard deviation of v.
+    
+    rho : float, -1 < rho < 1.
+        correlation between u and v.
+    
+    Returns
+    -------
+    None
+    
+    Notes
+    -----
+    * This function plots nine concentric elipses assoicated with 
+        probabilities of capture of {0.1, 0.2, ..., 0.9}.
+    
+    * The equations for x and y come from the bottom of Page 2 in [3].
+    
+    """
+    for i, p in enumerate(np.arange(9, 0, -1)/10.0):
+        r = np.sqrt(-2.0*(np.log(1-p)))
+        x = r*sigma_u * np.cos(THETA) + mu_u
+        y = r*sigma_v*(rho*np.cos(THETA) + np.sqrt(1 - rho*rho)*np.sin(THETA)) + mu_v
+        plt.fill(x, y, COLOR[i]);
+    plt.axis('equal')
+
+    
+def compute_cdf(mu_u, mu_v, sigma_u, sigma_v, rho, u, v):
+    """Compute the Mahalanobis cdf for [u, v] using a bivariate normal
+    distribution.
+    
+    Arguments
+    ---------
+    mu_u : float
+        mean of u.
+    
+    mu_v : float
+        mean of v.
+    
+    sigma_u : float, u > 0.
+        standard deviation of u.
+    
+    sigma_v : float, v > 0.
+        standard deviation of v.
+    
+    rho : float, -1 < rho < 1.
+        correlation between u and v.
+
+    Notes
+    -----
+    * The equations for the Mahalanobis distance, d_sqr, comes from
+        the bottom of Page 2 in [3].
+
+    * The Mahalanobis distance, d_sqr, follows the chi-squared distribution 
+        with 2 degrees of freedom.
+
+    * The equation for the returned cdf comes for the "Normal 
+        Distribution" section of [2].         
+
+    """
+    U = (u - mu_u)/sigma_u
+    V = (v - mu_v)/sigma_v
+    d_sqr = 1.0/(1.0 - rho*rho) * (U*U - 2*U*V + V*V)
+    return 1.0 - np.exp(-d_sqr/2.0)
