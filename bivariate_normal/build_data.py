5b62107ece962df98b46a0ff1ebd584bcd54e85e
Randal Barnes <barne003@umn.edu>
2022-08-05 09:30:23 -0500

    added experiments and restored the experimentation harness

--- bivariate_normal/build_data.py
+++ bivariate_normal/build_data.py
@@ -121,6 +121,9 @@ def build_hurricane_data(data_path, settings, verbose=0):
     * No scaling or normalization is applied during data extraction.
 
     """
+    if settings["uncertainty_type"] != 'bivariate_normal':
+        raise NotImplementedError
+    
     # Setup for the selected target.
     x_names = [
         "NCT",
