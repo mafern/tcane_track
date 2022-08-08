64e8fed42a838e39f1d201d2546067407f5e7361
Elizabeth Barnes <eabarnes@colostate.edu>
2022-08-08 07:09:34 -0700

    new metrics dictionary and comparisons

--- bivariate_normal/experiment_settings.py
+++ bivariate_normal/experiment_settings.py
@@ -247,7 +247,7 @@ def get_settings(experiment_name):
             "x_names": None,
         },     
         
-        "bivariate_normal_105_AL120": {
+        "bivariate_normal_205_AL120": {
             "filename": "nnfit_vlist_02-Jun-2022.dat",
             "uncertainty_type": 'bivariate_normal',  
             "leadtime": 120,
---
+++ bivariate_normal/prediction_metrics.py
@@ -0,0 +1,60 @@
+"""Custom metrics for model performance evaluation."""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import mahalanobis
+from scipy.stats import multivariate_normal
+
+__author__ = "Randal J Barnes and Elizabeth A. Barnes"
+__version__ = "08 August 2022"
+
+
+
+
+def get_errors(y_data, onehot_data):
+    error_vec = np.zeros((y_data.shape[0],2))
+    for isample in np.arange(0,y_data.shape[0]):
+        mu_u,mu_v,sigma_u,sigma_v,rho = (y_data[isample,0],
+                                         y_data[isample,1],
+                                         y_data[isample,2],
+                                         y_data[isample,3],
+                                         y_data[isample,4],
+                                        )
+        cov = np.array([[sigma_u**2, rho*sigma_v*sigma_u], [rho*sigma_v*sigma_u, sigma_v**2]])
+        rv = multivariate_normal([mu_u, mu_v], cov)
+        pred_x, pred_y = (rv.mean[0], rv.mean[1])
+        error = [pred_x - onehot_data[isample,0], pred_y - onehot_data[isample,1]]
+        error_vec[isample] = error
+
+    rmse = np.sqrt(error_vec[:,0]**2 + error_vec[:,1]**2)
+    rmse_cons = np.sqrt(onehot_data[:,0]**2 + onehot_data[:,1]**2)
+    
+    # perc_improve = 100*(rmse.mean()-rmse_cons.mean())/rmse_cons.mean()
+    # print('    ' + str(perc_improve.round(2)) + '%') # negative implies improvement
+    
+    return rmse, rmse_cons
+
+
+def compute_pit(y_data, onehot_data):
+    bins = np.linspace(0, 1, 11)
+    bins_inc = bins[1]-bins[0]
+
+    F = mahalanobis.compute_cdf(y_data[:,0], 
+                                y_data[:,1], 
+                                y_data[:,2], 
+                                y_data[:,3], 
+                                y_data[:,4], 
+                                onehot_data[:,0], 
+                                onehot_data[:,1],
+                               )
+    pit_hist = np.histogram(F,
+                            bins,
+                            weights=np.ones_like(F)/float(len(F)),
+                             )
+
+    B   = len(pit_hist[0])
+    D   = np.sqrt(1/B * np.sum( (pit_hist[0] - 1/B)**2 ))
+    EDp = np.sqrt( (1.-1/B) / (onehot_data.shape[0]*B) )
+    
+    return bins, pit_hist, D, EDp
+    
