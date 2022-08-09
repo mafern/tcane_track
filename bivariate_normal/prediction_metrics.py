ff52972a6af5a5e476358162faa8648d1e675d19
Elizabeth Barnes <eabarnes@colostate.edu>
2022-08-09 11:23:05 -0700

    ran a bunch of new experiments for comparisons.

--- bivariate_normal/prediction_metrics.py
+++ bivariate_normal/prediction_metrics.py
@@ -9,28 +9,40 @@ __author__ = "Randal J Barnes and Elizabeth A. Barnes"
 __version__ = "08 August 2022"
 
 
+def get_mv_normal(y_data):
+    mu_u,mu_v,sigma_u,sigma_v,rho = (y_data[0],
+                                     y_data[1],
+                                     y_data[2],
+                                     y_data[3],
+                                     y_data[4],
+                                    )
+    cov = np.array([[sigma_u**2, rho*sigma_v*sigma_u], [rho*sigma_v*sigma_u, sigma_v**2]])
+    rv = multivariate_normal([mu_u, mu_v], cov)
+    
+    return rv
+
+
+def compute_nll(y_data, onehot_data):
+    nll_vec = np.zeros((y_data.shape[0],))
+    
+    for isample in np.arange(0,y_data.shape[0]):
+        rv = get_mv_normal(y_data[isample,:])
+        nll = -rv.logpdf([onehot_data[isample,0],onehot_data[isample,1]])
+        nll_vec[isample] = nll
+    
+    return nll_vec
 
 
 def get_errors(y_data, onehot_data):
     error_vec = np.zeros((y_data.shape[0],2))
     for isample in np.arange(0,y_data.shape[0]):
-        mu_u,mu_v,sigma_u,sigma_v,rho = (y_data[isample,0],
-                                         y_data[isample,1],
-                                         y_data[isample,2],
-                                         y_data[isample,3],
-                                         y_data[isample,4],
-                                        )
-        cov = np.array([[sigma_u**2, rho*sigma_v*sigma_u], [rho*sigma_v*sigma_u, sigma_v**2]])
-        rv = multivariate_normal([mu_u, mu_v], cov)
+        rv = get_mv_normal(y_data[isample,:])
         pred_x, pred_y = (rv.mean[0], rv.mean[1])
         error = [pred_x - onehot_data[isample,0], pred_y - onehot_data[isample,1]]
         error_vec[isample] = error
 
     rmse = np.sqrt(error_vec[:,0]**2 + error_vec[:,1]**2)
     rmse_cons = np.sqrt(onehot_data[:,0]**2 + onehot_data[:,1]**2)
-    
-    # perc_improve = 100*(rmse.mean()-rmse_cons.mean())/rmse_cons.mean()
-    # print('    ' + str(perc_improve.round(2)) + '%') # negative implies improvement
     
     return rmse, rmse_cons
 
@@ -57,4 +69,27 @@ def compute_pit(y_data, onehot_data):
     EDp = np.sqrt( (1.-1/B) / (onehot_data.shape[0]*B) )
     
     return bins, pit_hist, D, EDp
+
+
+# def get_errors(y_data, onehot_data):
+#     error_vec = np.zeros((y_data.shape[0],2))
+#     for isample in np.arange(0,y_data.shape[0]):
+#         mu_u,mu_v,sigma_u,sigma_v,rho = (y_data[isample,0],
+#                                          y_data[isample,1],
+#                                          y_data[isample,2],
+#                                          y_data[isample,3],
+#                                          y_data[isample,4],
+#                                         )
+#         cov = np.array([[sigma_u**2, rho*sigma_v*sigma_u], [rho*sigma_v*sigma_u, sigma_v**2]])
+#         rv = multivariate_normal([mu_u, mu_v], cov)
+#         pred_x, pred_y = (rv.mean[0], rv.mean[1])
+#         error = [pred_x - onehot_data[isample,0], pred_y - onehot_data[isample,1]]
+#         error_vec[isample] = error
+
+#     rmse = np.sqrt(error_vec[:,0]**2 + error_vec[:,1]**2)
+#     rmse_cons = np.sqrt(onehot_data[:,0]**2 + onehot_data[:,1]**2)
     
+#     # perc_improve = 100*(rmse.mean()-rmse_cons.mean())/rmse_cons.mean()
+#     # print('    ' + str(perc_improve.round(2)) + '%') # negative implies improvement
+    
+#     return rmse, rmse_cons
