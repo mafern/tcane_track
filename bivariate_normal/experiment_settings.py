d29e652b5310d1b6918da96c39c791cbede4f233
Elizabeth Barnes <eabarnes@colostate.edu>
2022-08-10 07:52:57 -0700

    undid changes for leave-two-out, not helpful and made code messier

--- bivariate_normal/experiment_settings.py
+++ bivariate_normal/experiment_settings.py
@@ -277,33 +277,7 @@ def get_settings(experiment_name):
         
         
         #---------------------------------------------
-        # Tests
-        
-        "bivariate_normal_2203_AL72": {
-            "filename": "nnfit_vlist_02-Jun-2022.dat",
-            "uncertainty_type": 'bivariate_normal',  
-            "leadtime": 72,
-            "basin": "AL",
-            "undersample": False,
-            "hiddens": [5, 5],
-            "dropout_rate": [0.,0.,0.],
-            "ridge_param": [0.0,0.0],            
-            "learning_rate": 0.0001,
-            "momentum": 0.9,
-            "nesterov": True,
-            "batch_size": 64,
-            "rng_seed_list": [123, 234, 345],
-            "rng_seed": None,
-            "act_fun": "relu",
-            "n_epochs": 25_000,
-            "patience": 250,
-            "test_condition": "leave-two-out",
-            "years_test": None,
-            "val_condition": "random",
-            "n_val": 200,
-            "n_train": "max",
-            "x_names": None,
-        },           
+        # Tests        
         
         
         
