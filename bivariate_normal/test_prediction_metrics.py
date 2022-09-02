57088673541ef2096c6e682f063ed1b0f300787b
Randal Barnes <barne003@umn.edu>
2022-09-02 09:58:54 -0500

    covert to pytest

--- bivariate_normal/test_prediction_metrics.py
+++ bivariate_normal/test_prediction_metrics.py
@@ -3,28 +3,29 @@
 # date: September 2, 2022
 
 import numpy as np
-import prediction_metrics 
+import pytest
+
+import prediction_metrics
 
 
 def test_get_errors():
-    
+
     # test for when predictions and labels are equal
     y_data = np.ones((100,5))
     y_data[:,-1] = 0.
     onehot_data = np.ones((100,2))
     rmse, rmse_cons = prediction_metrics.get_errors(y_data, onehot_data)
-    
-    assert np.sum(rmse) == 0.0, "perfect predictions should have zero rmse"
 
-    # test for when predictions and labels are notequal    
+    assert np.all(rmse == 0.0), "perfect predictions should have zero rmse"
+
+    # test for when predictions and labels are notequal
     y_data = np.ones((100,5))
     y_data[:,-1] = 0.
     onehot_data = 3.*np.ones((100,2))
     rmse, rmse_cons = prediction_metrics.get_errors(y_data, onehot_data)
-    
-    assert np.sum(rmse) != 0.0, "should get nonzero errors for wrong predictions"
-    
-    
-    
-if __name__ == "__main__":    
-    test_get_errors()    
+
+    assert np.all(rmse != 0.0), "should get nonzero errors for wrong predictions"
+
+
+if __name__ == "__main__":
+    test_get_errors()
