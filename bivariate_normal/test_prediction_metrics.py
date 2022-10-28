26daea3f9687b230dfc07cc6d60b838df1c9730a
Elizabeth Barnes <eabarnes@colostate.edu>
2022-10-28 14:07:59 -0600

    modified unit tests to use unittest

--- bivariate_normal/test_prediction_metrics.py
+++ bivariate_normal/test_prediction_metrics.py
@@ -4,33 +4,58 @@
 
 import numpy as np
 import prediction_metrics
+import unittest
 
 
-def test_get_errors():
+# def test_correct_prediction_errors():
+#
+#     # test for when predictions and labels are equal
+#     y_data = np.ones((100,5))
+#     y_data[:,-1] = 0.
+#     onehot_data = np.ones((100,2))
+#     rmse, rmse_cons = prediction_metrics.get_errors(y_data, onehot_data)
+#
+#     assert np.all(rmse == 0.0), "perfect predictions should have zero rmse"
+#
+# def test_incorrect_prediction_errors():
+#     # test for when predictions and labels are notequal
+#     y_data = np.ones((100,5))
+#     y_data[:,-1] = 0.
+#     onehot_data = 3.*np.ones((100,2))
+#     rmse, rmse_cons = prediction_metrics.get_errors(y_data, onehot_data)
+#
+#     assert np.all(rmse != 0.0), "should get nonzero errors for wrong predictions"
 
-    # test for when predictions and labels are equal
-    y_data = np.ones((100, 5))
-    y_data[:, -1] = 0.0
-    onehot_data = np.ones((100, 2))
-    rmse, rmse_cons = prediction_metrics.get_errors(y_data, onehot_data)
 
-    assert np.all(rmse == 0.0), "perfect predictions should have zero rmse"
+# Using the unittest class
+class test_get_errors_unittest(unittest.TestCase):
+    def test_correct_prediction_errors_unittest(self):
+        # test for when predictions and labels are equal
+        y_data = np.ones((100, 5))
+        y_data[:, -1] = 0.
+        onehot_data = np.ones((100, 2))
+        rmse, rmse_cons = prediction_metrics.get_errors(y_data, onehot_data)
 
-    # test for when predictions and labels are notequal
-    y_data = np.ones((100, 5))
-    y_data[:, -1] = 0.0
-    onehot_data = 3.0 * np.ones((100, 2))
-    rmse, rmse_cons = prediction_metrics.get_errors(y_data, onehot_data)
+        # assertEqual() to check that the rmse values are equal to zero
+        self.assertEqual(rmse.tolist(), np.zeros(np.shape(rmse)).tolist(), "perfect predictions should have zero rmse")
 
-    assert np.all(rmse != 0.0), "should get nonzero errors for wrong predictions"
+    def test_incorrect_prediction_errors_unittest(self):
+        # test for when predictions and labels are notequal
+        y_data = np.ones((100, 5))
+        y_data[:, -1] = 0.
+        onehot_data = 3. * np.ones((100, 2))
+        rmse, rmse_cons = prediction_metrics.get_errors(y_data, onehot_data)
+
+        # assertNotEqual() to check that the rmse values are not equal to zero
+        self.assertNotEqual(rmse.tolist(), 0.0, "should get nonzero errors for wrong predictions")
 
 
 def test_compute_nll():
 
     # test that the negative log likelihood is very small when the prediction has near zero uncertainty
     y_data = np.zeros((100, 5))
-    y_data[:, 2:4] = 0.00001
+    y_data[:, 2:4] = .00001
     onehot_data = np.zeros((100, 2))
     nll_vec = prediction_metrics.compute_nll(y_data, onehot_data)
 
-    assert np.all(nll_vec < 1.0e-5)
+    assert np.all(nll_vec < 1.e-5)
