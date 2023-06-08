48b0a8365bd426758effdcf76cbd96247b3a4bf3
Martin Fernandez <mafern@blondie.local>
2023-06-08 12:40:17 -0600

    extra save method for leave-two-out

--- compute_metrics.py
+++ compute_metrics.py
@@ -87,5 +87,7 @@ def save_metrics(
         d[k_key] = metrics_valtest[k]
 
     # save the dataframe
-    df = pd.DataFrame(data=d, index=[0])
+    # df = pd.DataFrame(data=d, index=[0])
+    # df.to_pickle(metric_filename)
+    df = pd.DataFrame(data=d).transpose()
     df.to_pickle(metric_filename)
