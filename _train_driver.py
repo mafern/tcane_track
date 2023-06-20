9adac3a4daa000677aad51b2967807bd10c98ee5
Martin Fernandez <mafern@blondie.local>
2023-06-20 10:14:12 -0600

    make train driver take user input to select the experiment to run, adjust saving paths for easier navigation

--- _train_driver.py
+++ _train_driver.py
@@ -3,79 +3,45 @@
 
 import time
 import numpy as np
+import warnings
 import silence_tensorflow.auto
 import tensorflow as tf
-
+import experiment_settings
 from train_experiments import train_experiments
-
-DATA_PATH = "data/"
-MODEL_PATH = "../track_martin/saved_models/default_l2o/"
-METRICS_PATH = "../track_martin/saved_metrics/default_l2o/"
-PREDICTIONS_PATH = "../track_martin/saved_predictions/default_l2o/"
+import os
 
 tf.config.set_visible_devices([], "GPU")  # turn-off tensorflow-metal if it is on
-np.warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
+warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
 
 __author__ = "Elizabeth A. Barnes, Randal J Barnes, and Mark DeMaria"
 __version__ = "13 December 2022"
 
-# List of experiments to run
-# EXP_NAME_LIST = (
-#     # "centered_bivariate_normal_000_EPCP24",
-#
-#     "centered_bivariate_normal_100_EPCP12",
-#     "centered_bivariate_normal_101_EPCP24",
-#     "centered_bivariate_normal_102_EPCP36",
-#     "centered_bivariate_normal_103_EPCP48",
-#     "centered_bivariate_normal_104_EPCP60",
-#     "centered_bivariate_normal_105_EPCP72",
-#     "centered_bivariate_normal_106_EPCP84",
-#     "centered_bivariate_normal_107_EPCP96",
-#     "centered_bivariate_normal_108_EPCP108",
-#     "centered_bivariate_normal_109_EPCP120",
-#
-#     "centered_bivariate_normal_200_AL12",
-#     "centered_bivariate_normal_201_AL24",
-#     "centered_bivariate_normal_202_AL36",
-#     "centered_bivariate_normal_203_AL48",
-#     "centered_bivariate_normal_204_AL60",
-#     "centered_bivariate_normal_205_AL72",
-#     "centered_bivariate_normal_206_AL84",
-#     "centered_bivariate_normal_207_AL96",
-#     "centered_bivariate_normal_208_AL108",
-#     "centered_bivariate_normal_209_AL120",
-#     )
+testing = experiment_settings.Experiments()
+print('index range and short names for experiments')
+for expname in testing.get_exp_list_short:
+    exp_inds = [index for index, exp_string in enumerate(testing.get_exp_list) if expname in exp_string]
+    print(np.min(exp_inds), '-', np.max(exp_inds)+1, expname)
 
-EXP_NAME_LIST = (
-    # "centered_bivariate_normal_000_EPCP24",
+# pick the experiment
+expname = str(input("which experiment to run?: "))
+assert np.isin(expname, testing.get_exp_list_short), "chosen experiment does not yet exist"
+# get the correct indices for these experiments
+exp_inds = [index for index, exp_string in enumerate(testing.get_exp_list) if expname in exp_string]
+EXP_NAME_VEC = testing.get_exp_list[np.min(exp_inds):np.max(exp_inds)+1]
+print(EXP_NAME_VEC)
 
-    "default_l2o_EP12",
-    "default_l2o_EP24",
-    "default_l2o_EP36",
-    "default_l2o_EP48",
-    "default_l2o_EP60",
-    "default_l2o_EP72",
-    "default_l2o_EP84",
-    "default_l2o_EP96",
-    "default_l2o_EP108",
-    "default_l2o_EP120",
+DATA_PATH = "data/"
+MODEL_PATH = os.path.join("saved_models", expname)+'/'
+METRICS_PATH = os.path.join("saved_metrics/", expname)+'/'
+PREDICTIONS_PATH = os.path.join("saved_predictions/", expname)+'/'
 
-    "default_l2o_AL12",
-    "default_l2o_AL24",
-    "default_l2o_AL36",
-    "default_l2o_AL48",
-    "default_l2o_AL60",
-    "default_l2o_AL72",
-    "default_l2o_AL84",
-    "default_l2o_AL96",
-    "default_l2o_AL108",
-    "default_l2o_AL120",
-    )
+if not isinstance(EXP_NAME_VEC, list):
+    EXP_NAME_VEC = [EXP_NAME_VEC]
 
 if __name__ == "__main__":
     start_time = time.time()
     train_experiments(
-        EXP_NAME_LIST,
+        EXP_NAME_VEC,
         DATA_PATH,
         MODEL_PATH,
         METRICS_PATH,
